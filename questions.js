6b2d0dbabf6548d8588eac65ebcf1f2556295905
JandreiSS <jandreisst@gmail.com>
2023-10-20 17:15:01 -0300

    First commit

---
+++ questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Sabendo que a soma dos ângulos internos de um quadrilátero é sempre igual a 360º, em um quadrilátero em específico, a medida dos seus ângulos internos é proporcional aos números 3, 5, 6, 10. O valor do menor ângulo é:",
+    answer: "B) 45º",
+    options: [
+      "A) 15º",
+      "B) 45º",
+      "C) 60º",
+      "D) 75º"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Sobre a classificação dos ângulos, marque a alternativa correta:",
+    answer: "C) Um ângulo é classificado como agudo quando a sua medida é menor do que 90º.",
+    options: [
+      "A) Um ângulo é classificado como reto quando ele possui medida menor ou igual a 90º.",
+      "B) Dois ângulos são complementares quando a soma deles é igual a 180º.",
+      "C) Um ângulo é classificado como agudo quando a sua medida é menor do que 90º.",
+      "D) Dois ângulos cuja soma é igual a 90º graus são conhecidos como ângulos obtusos."
+    ]
+  },
+    {
+    numb: 3,
+    question: "Fernando é um carpinteiro e está separando ripas de madeira de diversos comprimentos para construir estruturas triangulares. Dentre as seguintes opções de trios de ripas, a única capaz de formar um triângulo é"
+    ,
+    answer: "D) 3 cm, 4 cm, 5 cm",
+    options: [
+      "A) 3 cm, 7 cm, 11 cm",
+      "B) 6 cm, 4 cm, 12 cm",
+      "C) 7 cm, 9 cm, 18 cm",
+      "D) 3 cm, 4 cm, 5 cm"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Os polígonos podem ser classificados como convexos ou não convexos, regulares ou não regulares. A respeito dessa classificação, assinale a alternativa correta",
+    answer: "A) Um polígono é convexo quando qualquer segmento de reta, que possui extremidades em seu interior, não possui pontos fora dele.",
+    options: [
+      "A) Um polígono é convexo quando qualquer segmento de reta, que possui extremidades em seu interior, não possui pontos fora dele.",
+      "B) Um polígono é dito convexo quando possui todos os lados iguais.",
+      "C) Um polígono é dito convexo quando possui todos os ângulos iguais.",
+      "D) Um polígono é regular quando possui lados congruentes. "
+    ]
+  },
+    {
+    numb: 5,
+    question: "Um polígono convexo possui 25 lados. Qual é o número total de diagonais que esse polígono possui? ",
+    answer: "D) 275",
+    options: [
+      "A) 200",
+      "B) 225",
+      "C) 250",
+      "D) 275"
+    ]
+  },
+];
---
+++ script.js
@@ -0,0 +1,325 @@
+document.getElementById('question1').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var res1 = Number(document.getElementById('res1').value);
+
+    var conf1 = "Incorreto";
+
+    if(res1 == 75) {
+        conf1 = "Você Acertou"
+    }
+
+    document.getElementById('result1').textContent = conf1;
+});
+
+document.getElementById('question2').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var res1 = Number(document.getElementById('res2').value);
+
+    var conf1 = "Incorreto";
+
+    if(res1 == 64) {
+        conf1 = "Você Acertou"
+    }
+
+    document.getElementById('result2').textContent = conf1;
+});
+
+document.getElementById('question3').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var res1 = Number(document.getElementById('res3').value);
+
+    var conf1 = "Incorreto";
+
+    if(res1 == 28) {
+        conf1 = "Você Acertou"
+    }
+
+    document.getElementById('result3').textContent = conf1;
+});
+
+document.getElementById('question4').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var res1 = Number(document.getElementById('res4').value);
+
+    var conf1 = "Incorreto";
+
+    if(res1 == 3) {
+        conf1 = "Você Acertou"
+    }
+
+    document.getElementById('result4').textContent = conf1;
+});
+
+document.getElementById('question5').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var res1 = Number(document.getElementById('res5').value);
+
+    var conf1 = "Incorreto";
+
+    if(res1 == 132) {
+        conf1 = "Você Acertou"
+    }
+
+    document.getElementById('result5').textContent = conf1;
+});
+
+document.getElementById('question6').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var res1 = Number(document.getElementById('res6').value);
+
+    var conf1 = "Incorreto";
+
+    if(res1 == 16) {
+        conf1 = "Você Acertou"
+    }
+
+    document.getElementById('result6').textContent = conf1;
+});
+
+document.getElementById('question7').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var res1 = Number(document.getElementById('res7').value);
+
+    var conf1 = "Incorreto";
+
+    if(res1 == 3) {
+        conf1 = "Você Acertou"
+    }
+
+    document.getElementById('result7').textContent = conf1;
+});
+
+document.getElementById('question8').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var res1 = Number(document.getElementById('res8').value);
+
+    var conf1 = "Incorreto";
+
+    if(res1 == 1) {
+        conf1 = "Você Acertou"
+    }
+
+    document.getElementById('result8').textContent = conf1;
+});
+
+document.getElementById('question9').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var res1 = Number(document.getElementById('res9').value);
+
+    var conf1 = "Incorreto";
+
+    if(res1 == 4) {
+        conf1 = "Você Acertou"
+    }
+
+    document.getElementById('result9').textContent = conf1;
+});
+
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
+const option_list = document.querySelector(".option_list");
+const time_line = document.querySelector("header .time_line");
+const timeText = document.querySelector(".timer .time_left_txt");
+const timeCount = document.querySelector(".timer .timer_sec");
+
+start_btn.onclick = ()=>{
+    info_box.classList.add("activeInfo");
+}
+
+exit_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo");
+}
+
+continue_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo");
+    quiz_box.classList.add("activeQuiz");
+    showQuetions(0); 
+    queCounter(1); 
+    startTimer(15);
+    startTimerLine(0);
+}
+
+let timeValue =  15;
+let que_count = 0;
+let que_numb = 1;
+let userScore = 0;
+let counter;
+let counterLine;
+let widthValue = 0;
+
+const restart_quiz = result_box.querySelector(".buttons .restart");
+const quit_quiz = result_box.querySelector(".buttons .quit");
+
+// if restartQuiz button clicked
+restart_quiz.onclick = ()=>{
+    quiz_box.classList.add("activeQuiz");
+    result_box.classList.remove("activeResult"); 
+    que_count = 0;
+    que_numb = 1;
+    userScore = 0;
+    widthValue = 0;
+    showQuetions(que_count); 
+    queCounter(que_numb); 
+    clearInterval(counter);
+    clearInterval(counterLine); 
+    startTimer(timeValue); 
+    startTimerLine(widthValue); 
+    timeText.textContent = "Time Left"; 
+    next_btn.classList.remove("show");
+}
+
+quit_quiz.onclick = ()=>{
+    window.location.reload();
+}
+
+const next_btn = document.querySelector("footer .next_btn");
+const bottom_ques_counter = document.querySelector("footer .total_que");
+
+next_btn.onclick = ()=>{
+    if(que_count < questions.length - 1){ 
+        que_count++; 
+        que_numb++; 
+        showQuetions(que_count); 
+        queCounter(que_numb); 
+        clearInterval(counter); 
+        clearInterval(counterLine); 
+        startTimer(timeValue); 
+        startTimerLine(widthValue); 
+        timeText.textContent = "Time Left"; 
+        next_btn.classList.remove("show"); 
+    }else{
+        clearInterval(counter); 
+        clearInterval(counterLine); 
+        showResult(); 
+    }
+}
+
+function showQuetions(index){
+    const que_text = document.querySelector(".que_text");
+
+    let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
+    let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
+    que_text.innerHTML = que_tag;
+    option_list.innerHTML = option_tag; 
+    
+    const option = option_list.querySelectorAll(".option");
+
+    for(i=0; i < option.length; i++){
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+
+function optionSelected(answer){
+    clearInterval(counter);
+    clearInterval(counterLine); 
+    let userAns = answer.textContent; 
+    let correcAns = questions[que_count].answer; 
+    const allOptions = option_list.children.length;
+    
+    if(userAns == correcAns){ 
+        userScore += 1; 
+        answer.classList.add("correct"); 
+        answer.insertAdjacentHTML("beforeend", tickIconTag); 
+        console.log("Correct Answer");
+        console.log("Your correct answers = " + userScore);
+    }else{
+        answer.classList.add("incorrect");
+        answer.insertAdjacentHTML("beforeend", crossIconTag); 
+        console.log("Wrong Answer");
+
+        for(i=0; i < allOptions; i++){
+            if(option_list.children[i].textContent == correcAns){  
+                option_list.children[i].setAttribute("class", "option correct"); 
+                option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); 
+                console.log("Auto selected correct answer.");
+            }
+        }
+    }
+    for(i=0; i < allOptions; i++){
+        option_list.children[i].classList.add("disabled"); 
+    }
+    next_btn.classList.add("show"); 
+}
+
+function showResult(){
+    info_box.classList.remove("activeInfo"); 
+    quiz_box.classList.remove("activeQuiz"); 
+    result_box.classList.add("activeResult"); 
+    const scoreText = result_box.querySelector(".score_text");
+    if (userScore > 3){
+        let scoreTag = '<span>Parabéns! 🎉, Você tem <p>'+ userScore +'</p> de <p>'+ questions.length +'</p></span>';
+        scoreText.innerHTML = scoreTag;  
+    }
+    else if(userScore > 1){ 
+        let scoreTag = '<span>Legal 😎, Você tem <p>'+ userScore +'</p> de <p>'+ questions.length +'</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else{ 
+        let scoreTag = '<span>Poderia ter ido melhor 😐, Você tem <p>'+ userScore +'</p> de <p>'+ questions.length +'</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+}
+
+function startTimer(time){
+    counter = setInterval(timer, 1000);
+    function timer(){
+        timeCount.textContent = time;
+        time--; 
+        if(time < 9){ 
+            let addZero = timeCount.textContent; 
+            timeCount.textContent = "0" + addZero; 
+        }
+        if(time < 0){
+            clearInterval(counter); 
+            timeText.textContent = "Time Off"; 
+            const allOptions = option_list.children.length; 
+            let correcAns = questions[que_count].answer; 
+            for(i=0; i < allOptions; i++){
+                if(option_list.children[i].textContent == correcAns){ 
+                    option_list.children[i].setAttribute("class", "option correct"); 
+                    option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); 
+                    console.log("Time Off: Auto selected correct answer.");
+                }
+            }
+            for(i=0; i < allOptions; i++){
+                option_list.children[i].classList.add("disabled");
+            }
+            next_btn.classList.add("show"); 
+        }
+    }
+}
+
+function startTimerLine(time){
+    counterLine = setInterval(timer, 29);
+    function timer(){
+        time += 1; 
+        time_line.style.width = time + "px"; 
+        if(time > 549){ 
+            clearInterval(counterLine);
+        }
+    }
+}
+
+function queCounter(index){
+    let totalQueCounTag = '<span><p>'+ index +'</p> de <p>'+ questions.length +'</p> Perguntas</span>';
+    bottom_ques_counter.innerHTML = totalQueCounTag; 
+}
+
